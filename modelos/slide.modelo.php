689f108287b7d68181af916dbf5398523ce1a110
Mosses <musicmojica@hotmail.com>
2018-02-06 05:05:31 -0600

    test

--- modelos/slide.modelo.php
+++ modelos/slide.modelo.php
@@ -3,7 +3,7 @@ require_once "conexion.php";
 class ModeloSlide{
 static public function mdlMostrarSlide($tabla){
 	
-$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla");
+$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla order by orden ASC");
 
 		$stmt -> execute();
 
