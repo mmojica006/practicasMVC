49bf08e87a0903f164d1b6696f7383e8244f1613
Moises Mojica <musicmojica@gmail.com>
2018-03-06 18:15:34 -0600

    test

--- vistas/js/quienesSomos.js
+++ vistas/js/quienesSomos.js
@@ -3,7 +3,7 @@ $.post("ajax/nosotros.ajax.php", {"action": "urlServer"}, function(result){
 $.each($('#tab-mision img'),function() {
     var src = $(this).attr("src"); //$('#tab-mision img').attr('src');
 if (!result.includes("localhost")){
-    $(this).attr('src',result+src);
+    $(this).attr('src',result.slice(0,-1)+src);
 }
 });
 
